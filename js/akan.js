654dad165ec18dba7ad89d3a4440680f033d61f2
Abdullahi <abdullahi@Abdullahis-MacBook-Air.local>
2019-10-28 15:00:30 +0300

    declared function for date/month/year

--- js/akan.js
+++ js/akan.js
@@ -2,5 +2,10 @@
 var maleName= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw" ,"Kofi", "Kwame"];
 var femaleName= ["Akosua", "Adwoa", "Abeena", "Akua", "Yaa", "Afua", "Ama"];
 
-
-
+//create a function that would be be giving output based on HTML file
+    function giveOutput () {
+        //declare variable intouts
+        var date=parseInt(document.getElementById("date").value);
+        var month=parseInt(document.getElementById("month").value);
+        var year=parseIn(document.getElementById("year").value);
+    }
